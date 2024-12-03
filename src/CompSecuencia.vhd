53c0b4729aab57417fd5b49c2eaac8e82636e60e
marcosrechi <marcos.rechina@alumnos.upm.es>
2024-12-03 19:44:53 +0100

    Codigo de CompSecuencia
    Terminado (a falta de retoques) la entidad CompSecuencia

--- src/CompSecuencia.vhd
+++ src/CompSecuencia.vhd
@@ -45,8 +45,43 @@ entity CompSecuencia is
 end CompSecuencia;
 
 architecture Behavioral of CompSecuencia is
-
+    signal i : integer := 0; -- Indice del vector sec_generada
+    signal terminado : boolean := false; -- Indica si ha terminado la comparacion
+    signal flag_boton : boolean := false; -- Para evitar que un boton se lea dos veces
+    
 begin
-
-
+    process(sec_generada,boton_pulsado)
+    begin
+        -- Inicializamos las señales de salida a 0
+        if terminado = false then
+            exito <= '0';
+            error <= '0';
+            fin_comparacion <= '0';
+        else -- Si terminado = true entonces reinicio el indice
+            i <= 0;
+        end if;
+        
+        -- Para evitar que se lea dos veces el mismo boton
+        if boton_pulsado = 0 then -- Si no se detecta ningun boton la entrada seria cero
+            flag_boton <= false; -- Reseteo el flag si ya no se detecta ningun boton
+        end if;
+        
+        -- Comparacion de boton con la secuencia
+        if boton_pulsado /= 0 and flag_boton = false and terminado = false then
+        -- Si hay un boton pulsado, el flag esta desactivado y no se ha terminado la comparacion ya
+            flag_boton <= true; -- Activo el flag de boton
+            
+            if sec_generada(i) = 0 then
+                exito <= '1'; -- Comparacion con exito
+                fin_comparacion <= '1'; -- Ha terminado la comparacion
+                terminado <= true;
+            elsif boton_pulsado = sec_generada(i) then -- Si el boton pulsado coincide con el de la secuencia
+                i <= i + 1; -- Avanzamos a la siguiente posicion
+            else -- Si el boton pulsado no coincide con el de la secuencia
+                error <= '1'; -- En la comparacion habia un error
+                fin_comparacion <= '1'; -- Ha terminado la comparacion
+                terminado <= true;
+            end if;
+        end if;
+    end process;
 end Behavioral;
