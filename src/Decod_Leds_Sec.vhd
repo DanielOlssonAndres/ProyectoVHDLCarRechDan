c528683709795570cd0e5d280c1980ab54427b2f
DanielOlssonAndres <99800510+DanielOlssonAndres@users.noreply.github.com>
2024-12-04 00:26:56 +0100

    Decod_Leds_Sec

---
+++ src/Decod_Leds_Sec.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Esta entidad se encarga de recibir un número entero (integrer) y encender el led correspondiente a dicho entero
+
+entity Decod_Leds_Sec is
+    generic( NLEDS: integer := 4 );
+    port(
+        ledDeSecuencia  : in integer; -- Entero que simboliza el led que queremos encender
+        CLK             : in std_logic; -- Señal de reloj para sincronización
+        RESET           : in std_logic; -- Reset asíncrono que apaga todos los leds
+        led             : out std_logic_vector(NLEDS downto 1) := (others => '0') -- leds de salida     
+    );
+end Decod_Leds_Sec;
+
+architecture BEHAVIORAL of Decod_Leds_Sec is
+begin
+    asign: process(CLK)
+    begin
+        if RESET = '1' then 
+            for i in 1 to NLEDS loop
+                led(i) <= '0';
+            end loop;
+        elsif rising_edge(CLK) then
+            for i in 1 to NLEDS loop -- Ponemos todos los leds a 0 excepto el seleccionado
+                if i = ledDeSecuencia then
+                    led(i) <= '1';
+                else
+                    led(i) <= '0';
+                end if;
+            end loop;       
+        end if;
+    end process;
+end BEHAVIORAL;
---
+++ src/Decod_Leds_Sec_tb.vhd
@@ -0,0 +1,74 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Decod_Leds_Sec_tb is
+end Decod_Leds_Sec_tb;
+
+architecture Behavioral of Decod_Leds_Sec_tb is
+
+    component Decod_Leds_Sec is
+    generic( NLEDS: integer := 4 );
+    port(
+        ledDeSecuencia  : in integer; -- Entero que simboliza el led que queremos encender
+        CLK             : in std_logic; -- Señal de reloj para sincronización
+        RESET           : in std_logic; -- Reset asíncrono que apaga todos los leds
+        led             : out std_logic_vector(NLEDS downto 1) -- leds de salida     
+    );
+end component;
+
+    -- Señales para testbench
+        signal ledDeSecuencia  : integer := 0; 
+        signal CLK             : std_logic := '0';
+        signal RESET           : std_logic := '0'; 
+        signal led             : std_logic_vector(4 downto 1);   
+begin
+
+-- Unit Under Test
+uut: Decod_Leds_Sec
+    generic map(NLEDS => 4)
+    port map(
+        ledDeSecuencia => ledDeSecuencia,
+        CLK => CLK,
+        RESET => RESET,
+        led => led
+    );
+
+    -- Generador de reloj: T = 20 ns
+    CLK <= not CLK after 10 ns;
+    
+    -- Proceso de testbench
+    tbp: process
+    begin
+        -- Prueba LED1
+        ledDeSecuencia <= 1;
+        wait for 50 ns;
+        -- Prueba de RESET
+        RESET <= '1';
+        wait for 10 ns;
+        assert led(1) = '1'
+            report "RESET NO FUNCIONA"
+            severity error;
+        wait for 50 ns;
+        -- Prueba de LED2 (tras desactivación de RESET)
+        ledDeSecuencia <= 2;
+        wait for 50 ns;
+        RESET <= '0';
+        wait for 50 ns;
+        -- Prueba de LED3
+        ledDeSecuencia <= 3;
+        wait for 50 ns;
+        -- Prueba de LED4
+        ledDeSecuencia <= 4;
+        wait for 50 ns;
+        
+        -- Prueba si se manda un 0
+        ledDeSecuencia <= 0;
+        wait for 50 ns;    
+        
+        -- FIN DE SIMULACIÓN
+        assert false
+            report "[FIN]: TEST COMPLETADO CORRECTAMENTE"
+            severity failure;
+    end process;
+    
+end Behavioral;
