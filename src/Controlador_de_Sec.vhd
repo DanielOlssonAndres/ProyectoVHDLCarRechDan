3d78fa9be3b86d5cf89f4ab2ea1d5715c8a669a8
DanielOlssonAndres <99800510+DanielOlssonAndres@users.noreply.github.com>
2024-12-05 01:10:53 +0100

    Controlador_de_Sec TERMINADO

--- src/Controlador_de_Sec.vhd
+++ src/Controlador_de_Sec.vhd
@@ -1,13 +1,77 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
+use work.paquete_types.all;
 
 entity Controlador_de_Sec is
-
+    generic( TAMSEC: integer := 14 );
+    port(
+        secuencia        : in vec_integrer(0 to TAMSEC); -- Secuencia de entrada
+        emitir_elemento  : in std_logic; -- Evento que indica que un elemento de la secuencia debe emitirse
+        CLK              : in std_logic; -- Reloj del sistema
+        
+        elemento         : out integer; -- elemento de la secuencia que es emitido
+        fin_secuencia    : out std_logic; -- Salida que indica que se ha llegado al final de la secuencia de entrada
+        pedir_tiempo     : out std_logic -- Pide a un temporizador que empiece a contar
+    );
 end entity;
 
 architecture Behavioral of Controlador_de_Sec is
-
+   	type STATE is (ESPERANDO, EMITIENDO); 
+	signal cur_state      : STATE := ESPERANDO; 
+	signal nxt_state      : STATE := ESPERANDO;
+	signal fin_detectado  : std_logic := '0';
+	signal indice         : integer := 0;
 begin
 
+st_reg: process(CLK) -- Registro de estado
+    begin
+        if rising_edge(CLK) then
+        	cur_state <= nxt_state;
+        end if;
+    end process;
+    
+    nxt_st_dec: process(secuencia, fin_detectado)
+    begin
+    	case cur_state is
+        	when ESPERANDO =>
+                if secuencia'event then
+                    nxt_state <= EMITIENDO;
+                end if;
+            when EMITIENDO =>
+            	if fin_detectado = '1' then
+            	   nxt_state <= ESPERANDO;
+            	end if;
+            when others =>
+            	nxt_state <= ESPERANDO;
+        end case;
+    end process;
+    
+    out_dec: process(cur_state, secuencia, emitir_elemento)
+    begin
+    	case cur_state is
+        	when ESPERANDO =>
+        	    fin_secuencia <= '1';
+        	    fin_detectado <= '0';
+        	    pedir_tiempo <= '0';
+                elemento <= 0;
+                indice <= 0;
+            when EMITIENDO =>
+                fin_secuencia <= '0';
+                elemento <= secuencia(indice);
+                pedir_tiempo <= '1';
+                if secuencia(indice) = 0 then
+                    fin_detectado <= '1';
+                elsif (emitir_elemento = '1' and emitir_elemento'event) then
+                    indice <= indice + 1;
+                    pedir_tiempo <= '0';
+                end if;
+            when others =>
+        	    fin_secuencia <= '1';
+        	    fin_detectado <= '0';
+        	    pedir_tiempo <= '0';
+                elemento <= 0;
+                indice <= 0;
+        end case;
+    end process;
+    
 end architecture;
--- src/Controlador_de_Sec_tb.vhd
+++ src/Controlador_de_Sec_tb.vhd
@@ -1,12 +1,87 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.NUMERIC_STD.ALL;
+use work.paquete_types.all;
 
 entity Controlador_de_Sec_tb is
 end entity;
 
-architecture Behavioral of Controlador_de_Sec is
+architecture sim of Controlador_de_Sec_tb is
+
+component Controlador_de_Sec is
+    generic( TAMSEC: integer := 14 );
+    port(
+        secuencia        : in vec_integrer(0 to TAMSEC);
+        emitir_elemento  : in std_logic;
+        CLK              : in std_logic;
+        
+        elemento         : out integer;
+        fin_secuencia    : out std_logic;
+        pedir_tiempo     : out std_logic
+    );
+end component;
+
+signal secuencia       : vec_integrer(0 to 14) := (others => 0);
+signal emitir_elemento : std_logic := '0';
+signal CLK             : std_logic := '0';
+signal elemento        : integer;
+signal fin_secuencia   : std_logic;
+signal pedir_tiempo    : std_logic;
+
+constant CLK_T : time := 10 ns;
 
 begin
+    -- Unit Under Test
+    uut: Controlador_de_Sec
+        generic map ( TAMSEC => 14 )
+        port map (
+            secuencia => secuencia,
+            emitir_elemento => emitir_elemento,
+            CLK => CLK,
+            elemento => elemento,
+            fin_secuencia => fin_secuencia,
+            pedir_tiempo => pedir_tiempo
+        );
+
+    -- Generación del reloj
+    clkgen: process
+    begin
+        while true loop
+            CLK <= '1';
+            wait for CLK_T / 2;
+            CLK <= '0';
+            wait for CLK_T / 2;
+        end loop;
+    end process;
+
+    -- Simulación del temporizador
+    temp: process
+    begin
+        wait on pedir_tiempo;
+        if pedir_tiempo = '1' then
+            wait for 30 ns;
+            emitir_elemento <= '1';
+            wait for 10 ns;
+            emitir_elemento <= '0';
+        end if;
+    end process;
+
+    -- Estímulos
+    sim: process
+    begin
+        -- Primera secuencia
+        secuencia <= (others => 0);
+        wait for 50 ns;
+        secuencia <= (2, 3, 4, 1, 1, 4, 2, 3, others => 0);
+        wait for 1000 ns;
+
+        -- Nueva secuencia
+        secuencia <= (2, 3, 4, 3, 3, 2, 2, 1, 1, 3, 4, 2, others => 0);
+        wait for 1000 ns;
+
+        wait for 100 ns;
+        assert false 
+            report "FIN CORRECTO DE SIMULACION" 
+            severity failure;
+    end process;
 
 end architecture;
