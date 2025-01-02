a19062ed3b906037f6e271a30e1b774e4e8779aa
Carlotagutierrezz <carlota.gutierrez@alumnos.upm.es>
2025-01-02 21:43:52 +0100

    GenSecuencia

--- src/GenSecuencia_tb.vhd
+++ src/GenSecuencia_tb.vhd
@@ -6,111 +6,74 @@ end GenSecuencia_tb;
 
 architecture Behavioral of GenSecuencia_tb is
     
-    -- Declaración del componente a testear
     component GenSecuencia is
         port(
             -- Entradas
-            niv_actual        : in std_logic_vector(0 to 2); -- Indica en que nivel nos encontramos 
-            bot_accion        : in std_logic; -- Evento de inicio de generador
-            s_enable          : in std_logic; -- Si = 0, no se generan secuencias (enable)
-            CLK               : in std_logic; -- Reloj del sistema
-            -- Salidas
-            sec_generada      : out std_logic_vector(0 to 44); -- Secuencia generada
-            sec_lista         : out std_logic -- Indica que la nueva secuencia está lista en la salida
+           niv_actual        : in std_logic_vector(0 to 2); -- Indica en que nivel nos encontramos 
+           sec_generada      : out std_logic_vector(0 to 44) -- Secuencia generada
         );
     end component;
     
     -- Señales para el testbench
     signal niv_actual    : std_logic_vector(0 to 2); 
-    signal bot_accion    : std_logic; 
-    signal s_enable      : std_logic; 
-    signal CLK           : std_logic := '0';
-    signal sec_generada  : std_logic_vector(0 to 44);     
-    signal sec_lista     : std_logic;
+    signal sec_generada  : std_logic_vector(0 to 44);
 
 begin
-    -- Unit Under Test
     uut: GenSecuencia
         port map(
             niv_actual => niv_actual, 
-            bot_accion => bot_accion, 
-            s_enable => s_enable, 
-            CLK => CLK,
-            sec_generada => sec_generada,
-            sec_lista => sec_lista
+            sec_generada => sec_generada
         );    
 
-    -- Generador de reloj  
-   clk_gen: process 
-   begin
-       CLK <= '0';
-       wait for 5 ns;
-       CLK <= '1';
-       wait for 5 ns;
-   end process;
    
-    test: process
+     test: process
     begin
         -- Inicialización
         niv_actual <= "000";
-        bot_accion <= '0';
-        s_enable <= '0';
         wait for 20 ns;
 
-        -- Prueba generación secuencia en nivel 0
+        -- Prueba generación de secuencia para cada nivel
         niv_actual <= "000";
-        s_enable <= '1';
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
+        assert sec_generada = "010011001011000000000000000000000000000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel 000" severity error;
 
-        -- Prueba generación secuencia en nivel 1 si s_enable = 0
         niv_actual <= "001";
-        s_enable <= '0';
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
-
-        -- Prueba del resto de niveles
-        s_enable <= '1';
-        niv_actual <= "001";
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
-        wait for 20 ns;
+        assert sec_generada = "001011100010011001000000000000000000000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel 001" severity error;
 
         niv_actual <= "010";
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
+        assert sec_generada = "010011100001010100010011000000000000000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel 010" severity error;
 
         niv_actual <= "011";
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
+        assert sec_generada = "010100001010011010011100001010000000000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel 011" severity error;
 
         niv_actual <= "100";
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
+        assert sec_generada = "001100001011010100011010100011001010000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel 100" severity error;
 
         niv_actual <= "101";
-        bot_accion <= '1';
-        wait for 10 ns;
-        bot_accion <= '0';
         wait for 20 ns;
+        assert sec_generada = "011001010100100001011010100010011001100010011"
+            report "[ERROR]: Secuencia incorrecta para nivel 101" severity error;
+
+        -- Valor por defecto para nivel fuera de rango
+        niv_actual <= "111";
+        wait for 20 ns;
+        assert sec_generada = "000000000000000000000000000000000000000000000"
+            report "[ERROR]: Secuencia incorrecta para nivel fuera de rango" severity error;
 
         -- Fin de simulación
         wait for 20 ns;
         assert false
-            report "[FIN SIMULACION]: FINALIZADO CORRECTAMENTE"
+            report "[FIN SIMULACION]: Todas las pruebas pasaron correctamente"
             severity failure;
-            
     end process;
 
 end Behavioral;
