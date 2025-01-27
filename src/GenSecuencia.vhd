09a86e05e4020d0eda0a98ad86cc39b126bd9878
marcosrechi <marcos.rechina@alumnos.upm.es>
2025-01-28 00:32:15 +0100

    Cambio de carpeta de GenSecuencia
    Como mencionamos en la presentación, simplemente hemos cambiado el archivo de GenSecuencia de la carpetea src interna del proyecto a la carpeta src externa al proyecto

--- src/GenSecuencia.vhd
+++ src/GenSecuencia.vhd
@@ -1,45 +1,40 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use work.paquete_types.all;
+library ieee;
+use ieee.std_logic_1164.ALL;
 
 entity GenSecuencia is
     port(
         -- Entradas
-        niv_actual        : in integer; -- Indica en que nivel nos encontramos 
-        bot_accion        : in std_logic; -- Evento de inicio de generador
-        s_enable          : in std_logic; -- Si = 0, no se generan secuencias
+        niv_actual        : in std_logic_vector(0 to 2); -- Indica en que nivel nos encontramos 
         -- Salidas
-        sec_generada      : out vec_integrer(0 to 13) -- Secuencia generada
+        sec_generada      : out std_logic_vector(0 to 44) -- Secuencia generada
     );
 end GenSecuencia;
 
-architecture BEHAVIORAL of GenSecuencia is
+architecture Dataflow of GenSecuencia is
 
     -- Tabla de secuencias para cada nivel
-    constant semilla0 : vec_integrer(0 to 13) := (2, 3, 1, 3, others => 0);
-    constant semilla1 : vec_integrer(0 to 13) := (1, 1, 4, 2, 3, 1, others => 0);
-    constant semilla2 : vec_integrer(0 to 13) := (2, 3, 4, 1, 1, 4, 2, 3, others => 0);
-    constant semilla3 : vec_integrer(0 to 13) := (2, 4, 1, 2, 3, 2, 3, 4, 1, 2, others => 0);
-    constant semilla4 : vec_integrer(0 to 13) := (1, 1, 1, 3, 2, 4, 4, 2, 2, 3, 1, 2, others => 0);
-    constant semilla5 : vec_integrer(0 to 13) := (3, 3, 2, 4, 1, 3, 2, 4, 2, 3, 1, 1, 2, 3);
+    constant semilla0 : std_logic_vector(0 to 44) := ("010" & "011" & "001" & "011" & "000000000000000000000000000000000");
+    constant semilla1 : std_logic_vector(0 to 44) := ("001" & "011" & "100" & "010" & "011" & "001" & "000000000000000000000000000");
+    constant semilla2 : std_logic_vector(0 to 44) := ("010" & "011" & "100" & "001" & "010" & "100" & "010" & "011" & "000000000000000000000");
+    constant semilla3 : std_logic_vector(0 to 44) := ("010" & "100" & "001" & "010" & "011" & "010" & "011" & "100" & "001" & "010" & "000000000000000");
+    constant semilla4 : std_logic_vector(0 to 44) := ("001" & "100" & "001" & "011" & "010" & "100" & "011" & "010" & "100" & "011" & "001" & "010" & "000000000");
+    constant semilla5 : std_logic_vector(0 to 44) := ("011" & "001" & "010" & "100" & "001" & "011" & "010" & "100" & "010" & "011" & "001" & "100" & "010" & "011" & "000");
 
-    signal sec_generada_s : vec_integrer(0 to 13) := (others => 0);
+    signal sec_generada_s : std_logic_vector(0 to 44) := ("000000000000000000000000000000000000000000000");
 
 begin 
-
-    count: process(bot_accion)
-    begin
-        if (rising_edge(bot_accion) and (s_enable = '1')) then
-            case niv_actual is
-                when 0 => sec_generada_s <= semilla0;
-                when 1 => sec_generada_s <= semilla1;
-                when 2 => sec_generada_s <= semilla2;
-                when 3 => sec_generada_s <= semilla3;
-                when 4 => sec_generada_s <= semilla4;
-                when 5 => sec_generada_s <= semilla5;
-                when others => sec_generada_s <= (others => 0);
-            end case;
-        end if;
-    end process;
+    
+    with niv_actual select
+        sec_generada_s <= semilla0 when "000",
+                          semilla1 when "001",
+                          semilla2 when "010",
+                          semilla3 when "011",
+                          semilla4 when "100",                          
+                          semilla5 when "101",
+                          ("000000000000000000000000000000000000000000000") when others;
+                          
+    -- Asignaciones a las salidas
     sec_generada <= sec_generada_s;
-end BEHAVIORAL;
+
+end Dataflow;
+
